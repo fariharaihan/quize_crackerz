4134bc11fac696f730b9b77daaa4082818f275f2
fariharaihan <liafariha5@gmail.com>
2022-10-11 07:40:23 +0600

    router set

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './components/layout/Main';
+import Home from './components/Home/Home';
+import Topic from './components/Topic/Topic';
+import Statistic from './components/Statistic/Statistic';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+        {
+          path: '/topic',
+          element: <Topic></Topic>
+        },
+        {
+          path: '/statistic',
+          element: <Statistic></Statistic>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        },
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
