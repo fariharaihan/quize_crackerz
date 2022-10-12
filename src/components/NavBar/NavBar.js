bc4162705a19444b75c5a81a439ed9e0fa197922
fariharaihan <liafariha5@gmail.com>
2022-10-12 10:31:31 +0600

    navbar responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false);
     return (
         <div className='m-2' >
-            <header className="p-4 bg-slate-50 text-gray-800">
+            <header className=" p-4 bg-slate-50 text-gray-800">
                 <div className="container flex justify-between  mx-auto">
-                    <span className='ml-2 text-3xl font-bold tracking-wide text-blue-400 '>
-                        Quiz-Crackerz-Zone
-                    </span>
-
-                    <nav className='flex flex-wrap items-center space-x-4 sm:space-x-8'>
-
-                        <Link to='/'>Home</Link>
-                        <Link to='/topic'>Topic</Link>
-                        <Link to='/statistic'>Statistic</Link>
-                        <Link to='/blog'>Blog</Link>
+                    <nav className='flex items-center '>
+                        <ui className={`menu md:menu-horizontal md:static absolute duration-500 bg-slate-50  ease-in  ${open ? 'top-16' : 'top-[-200px]'}`}>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/topic'>Topic</Link></li>
+                            <li><Link to='/statistic'>Statistic</Link></li>
+                            <li><Link to='/blog'>Blog</Link></li>
+                        </ui>
+                        <div onClick={() => setOpen(!open)} className="h-6 w-6 md:hidden" >
+                            {
+                                open ? <XMarkIcon /> : <Bars3Icon />
+                            }
+                        </div>
                     </nav>
+                    <span className='ml-2 text-3xl font-bold tracking-wide text-blue-400 '>
+                        Quiz-Class
+                    </span>
                 </div>
             </header>
 
