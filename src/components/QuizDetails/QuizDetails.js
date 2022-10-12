d6edac25693e4fdf6a6e62c4f9c2d2e119a44ad8
fariharaihan <liafariha5@gmail.com>
2022-10-12 12:06:59 +0600

    correct answer add

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuizOption from '../QuizOption/QuizOption';
 import { toast } from 'react-toastify';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
+
 
 
 const QuizDetails = ({ quizTopic }) => {
-    console.log(quizTopic)
-    const { question, correctAnswer, options, id } = quizTopic
+    const [open, setOpen] = useState(true);
+    const { question, correctAnswer } = quizTopic;
 
 
 
     const handleAns = (correct) => {
         if (correct === correctAnswer) {
-            toast.success('Curert answer', { autoClose: 500 })
+            toast.success('Correct answer', { autoClose: 700 })
         }
         else {
-            toast.warning('Incorrent answer!', { autoClose: 500 })
+            toast.warning('Incorrent answer!', { autoClose: 700 })
         }
     }
     return (
-        <div className='border-4 border-sky-500 rounded-md text-white py-5'>
+        <div className='border-4 border-sky-400 rounded-md text-white py-5'>
             <div className='question-section'>
-                {/* <div className='question-count'>
-                    <span>Question</span>
-                </div> */}
-                <div className='flex justify-between px-5'>
-                    <h1 className='text-2xl text-white'>Question : {question}</h1>
 
+                <div className='flex justify-between'>
+                    <h1 className='text-2xl text-white ml-10'>Question : {question}</h1>
+
+                    <div onClick={() => setOpen(!open)} className='h-6 w-6 mr-10 text-blue-300'>
+                        {open ? <EyeIcon /> : <EyeSlashIcon></EyeSlashIcon>}
+                    </div>
                 </div>
+                <div className={open ? 'hidden' : 'block'}>
+                    <h1 className='text-xl py-2 bg-black px-2 rounded-sm my-5'>
+                        <span className='text-sky-500 text-2xl'> Answer : </span>
+                        {correctAnswer}
+                    </h1>
+                </div>
+
             </div>
 
             <div >
--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ import 'react-toastify/dist/ReactToastify.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <ToastContainer position='top-center' />
+    <>
+      <ToastContainer position='top-center' />
+      <App />
+    </>
+
   </React.StrictMode>
 );
 
